40717b754b6984783a41edf37beec45726e33f11
Jalah07 <jalahmckillion012@gmail.com>
2024-05-09 16:20:18 -0500

    Fix node

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,9 +2,10 @@ const Sauce = require('../models/sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-
+    const sauceObject =  JSON.parse(req.body.sauce);
+    delete sauceObject._id; 
     const sauce = new Sauce({
-        ...JSON.parse(req.body.sauce),
+        ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         userId: req.user._id,
     });
@@ -13,7 +14,6 @@ exports.createSauce = (req, res, next) => {
             res.status(201).json({
                 message: 'Post saved successfully!'
             });
-            console.log(sauce);
         }
     ).catch(
         (error) => {
@@ -22,9 +22,8 @@ exports.createSauce = (req, res, next) => {
             });
         }
     )
-    console.log(sauce);
 }
-//TODO CAP the sauce
+
 exports.getSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
@@ -32,7 +31,6 @@ exports.getSauce = (req, res, next) => {
 }
 
 exports.getSauces = (req, res, next) => {
-    console.log("Getting sauces")
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }))
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const controller = require('../controllers/sauce');
-//const auth = require()
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post("/", controller.createSauce);
-router.get("/:id", controller.getSauce);
-router.get("/", controller.getSauces);
-router.put("/:id", controller.updateSauce);
-router.delete("/:id", controller.deleteSauce);
-router.post("/:id/like", controller.likeSauce);
+router.post("/", auth, multer, controller.createSauce);
+router.get("/:id", auth, controller.getSauce);
+router.get("/", auth, controller.getSauces);
+router.put("/:id", auth, multer, controller.updateSauce);
+router.delete("/:id", auth, controller.deleteSauce);
+router.post("/:id/like", auth, controller.likeSauce);
 
 module.exports = router;
